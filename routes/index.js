a6f3fe792edc1c1ea696abaca6c8199711572e81
Alexandre M. Bonomi <alexandrembonomi@gmail.com>
2019-07-26 16:04:46 -0300

    email set for presentation

--- routes/index.js
+++ routes/index.js
@@ -52,27 +52,27 @@ router.post('/', (req, res, next) => {
     Cupom.create({nome, sobrenome, email, idade, receita, codigo})
     .then(cupom => {  
 
-    // let transporter = nodemailer.createTransport({
-      //   host: "smtp.sparkpostmail.com",
-      //   port: 587,
-      //   auth: {
-      //     type: 'AUTH PLAIN',
-      //     user: process.env.MAIL_USER,
-      //     pass: process.env.MAIL_PASS,
-      //   }
-      // });
-
-      let transporter = nodemailer.createTransport({
-        host: "smtp.mailtrap.io",
-        port: 2525,
+    let transporter = nodemailer.createTransport({
+        host: "smtp.sparkpostmail.com",
+        port: 587,
         auth: {
-          user: "ab6ff763f83810",
-          pass: "72ed7d5f04aee2"
+          type: 'AUTH PLAIN',
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASS,
         }
       });
 
+      // let transporter = nodemailer.createTransport({
+      //   host: "smtp.mailtrap.io",
+      //   port: 2525,
+      //   auth: {
+      //     user: "ab6ff763f83810",
+      //     pass: "72ed7d5f04aee2"
+      //   }
+      // });
+
       transporter.sendMail({
-        from: '<beet@ironhackers.dev>',
+        from: '<beet@gin.ink>',
         to: email, 
         subject: 'Você acaba de ganhar um cupom!', 
         text: `${process.env.BASE_URL}/cupom/${codigo}`,
